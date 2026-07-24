d138cdca2722961b92223291799c7245aa19c752
mohammad <mh177148@gmail.com>
2026-07-25 02:41:04 +0330

    fead(table): create table

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { CreateTableDto } from './dto/create-table.dto';
+import { Roles } from 'src/auth/roles.decorator';
+import { TableService } from './table.service';
 
 @Controller('table')
-export class TableController {}
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+export class TableController {
+    constructor(private tableService: TableService) { }
+
+
+    @Post('table')
+    @Roles('admin')
+    createTable(@Body() createTableDto: CreateTableDto) {
+        return this.tableService.CreateTable(createTableDto)
+    }
+}
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Table } from './entities/table.entity';
+import { Repository } from 'typeorm';
+import { CreateTableDto } from './dto/create-table.dto';
+import { TableStatus } from './table-status.enum';
 
 @Injectable()
-export class TableService {}
+export class TableService {
+    constructor(
+        @InjectRepository(Table)
+        private tableRepository: Repository<Table>
+    ) { }
+
+
+    async CreateTable(createTableDto: CreateTableDto) {
+        const { number } = createTableDto
+
+        const existing = await this.tableRepository.findOne({ where: { number } })
+
+        if (existing) {
+            throw new ConflictException('میز با این شماره وجود دارد')
+        }
+
+        const table = this.tableRepository.create({
+            ...createTableDto,
+            status: createTableDto.status || TableStatus.AVAILABLE
+        })
+
+        try {
+            return await this.tableRepository.save(table)
+
+        } catch (error) {
+            throw new InternalServerErrorException()
+        }
+    }
+}
