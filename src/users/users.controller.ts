8108fbd11744e4b1c5579d8049c2291292865364
mohammad <mh177148@gmail.com>
2026-07-28 18:40:29 +0330

    fead(users): getAllUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { UsersService } from './users.service';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('users')
-export class UsersController {}
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+export class UsersController {
+    constructor(private usersService: UsersService) { }
+
+
+    @Get()
+    @Roles('admin')
+    getAllUsers() {
+        this.usersService.getAllUsers()
+    }
+    
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) { }
+
+
+    async getAllUsers(): Promise<User[]> {
+        const users = await this.userRepository.find({
+            where: { isActive: true },
+        })
+
+        return users
+    }
+}
